8df19615a0214d84fe005ee5d4c311d805de9ed9
JaarmaCo <williamh.jaarma@gmail.com>
2026-09-04 20:25:28 +0200

    feat: Restructure with partially complete generator

--- src/c-template.py
+++ src/c-template.py
@@ -1,105 +1,171 @@
-import re
-import json
 import argparse
-import subprocess
-from typing import Any, Callable, Iterable
+import json
+import re
+from sys import stderr, stdin
+from types import SimpleNamespace
+from typing import Any
 
-DIRECTIVE = re.compile(r"^\s*//\s*!\s*Template\s+([A-Z]+)\s+(.*)\s*$", flags=re.MULTILINE)
-ATTRIBUTES = [ "TYPE", "MANGLE" ]
-HIDDEN = [ "TYPE", "MANGLE" ]
-FILE_DIRECTIVES = [ "C", "H" ]
+DIR = re.compile(r"//\s*!\s*Template\s+([A-Z]+)\s+(.*)")
+TYPEDEF = re.compile(r"\s*typedef\s+(.*)\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*;\s*", flags=re.MULTILINE)
+DEFINE = re.compile(r"\s*#\s*define\s+([a-zA-Z_][a-zA-Z0-9_]*)\s*(.*)\s*$\s*", flags=re.MULTILINE)
 
-def process_directives(source: str, processor: Callable[[str, Any, re.Match[str], str], str]) -> str:
+def read_directives(source, dir_callback):
     i = 0
-    while True:
-        i = i + 1
-
-        m = DIRECTIVE.search(source)
+    while i < len(source):
+        m = DIR.match(source, i)
         if m is None:
-            break
-        next = processor(m.group(1), json.loads(m.group(2)), m, source)
-        source = next
-    return source
+            i += 1
+            continue
+        dir_callback(m)
+        i = m.end()
 
-TYPEDEF = re.compile(r"typedef\s+(.*)\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*;")
+def lineo(m: re.Match[str], src: str):
+    col = 1
+    line = 1
+    for i in range(0, m.start()):
+        if src[i] == '\n':
+            line += 1
+            col = 1
+        else:
+            col += 1
+    return f"{col}:{line}"
 
-def get_type_parameters(args, directives, elements) -> dict[int, str]:
+def erase_all(source, regions):
+    result = ""
+    start = 0
+    for i, j in regions:
+        result += source[start:i]
+        start = j
+
+    result += source[start:]
+    return result
+
+def parse_types(args, source):
 
     types = {}
+    erase_regions = []
 
-    if 'TYPE' not in directives or 'TYPE' not in elements:
-        return types
+    def parser(m: re.Match[str]):
+        if m.group(1) != 'TYPE':
+            return
+ 
+        index = int(m.group(2))
+        if index > len(args.types):
+            print(f"Defaulting unspecified argument {index} to int ({lineo(m, source)})", file=stderr)
+            targ = "int"
+        else:
+            targ = args.types[index - 1]
 
-    for number, element in zip(directives['TYPE'], elements['TYPE']):
+        typedef = TYPEDEF.search(source, m.end())
+        if typedef is None:
+            print(f"TYPE directive should annotate a typedef at {lineo(m, source)}", file=stderr)
+            return
 
-        match = TYPEDEF.match(element)
-        if match is None:
-            print(f"Invalid template directive TYPE={number}: {element}")
+        erase_regions.append((m.start(), typedef.end()))
+
+        tname = typedef.group(2)
+        types[tname] = targ
+
+
+    read_directives(source, parser)
+    return erase_all(source, erase_regions), types
+
+def seek_paren(source, i, oparen, cparen):
+    level = 0
+    while i < len(source):
+        if source[i] == oparen:
+            level += 1
+        elif source[i] == cparen:
+            level -= 1
+
+        if level == 0:
+            return i + 1
+        i += 1
+    return len(source)
+
+COMMANDS = {
+    "toupper": lambda params, values: " ".join(params[1:]).upper(),
+    "tolower": lambda params, values: " ".join(params[1:]).lower(),
+}
+
+def run_cmd(cmd, values):
+    params = cmd.split(' ')
+    if params[0] in COMMANDS:
+        return COMMANDS[params[0]](params, values)
+    return ""
+
+def expand_env(source, values):
+    i = 0
+    subst = ""
+    while i < len(source):
+
+        if source[i] != '$':
+            subst += source[i]
+            i += 1
             continue
 
-        name = match.group(2)
-        types[number] = name
+        if i + 2 >= len(source):
+            subst += source[i]
+            i += 1
+            continue
 
-    return types
+        if source[i + 1] == '{':
+            end = seek_paren(source, i + 1, '{', '}')
+            key = expand_env(source[i + 2 : end - 1], values)
+            keys = key.split('.')
+            value = values
+            for part in keys:
+                if part in value:
+                    value = value[part]
+                else:
+                    break
+            subst += value
+            i = end
+        elif source[i + 1] == '(':
+            end = seek_paren(source, i + 1, '(', ')')
+            cmd = expand_env(source[i + 2 : end - 1], values)
+            subst += run_cmd(cmd, values)
+            i = end
 
-ENVAR = re.compile(r"\$\{([^}]+)\}")
+    return subst
 
-def subst_env(pattern: str, env: dict[str, Any]) -> str:
-
-    def subst_var(name: str) -> str:
-        keys = name.split(".")
-        value = env
-        for key in keys:
-            if key not in value:
-                break
-            value = value[key]
-        return str(value)
-
-    return re.sub(ENVAR, lambda match: subst_var(match.group(1)), pattern)
-
-MANGLE = re.compile(r"#\s*define\s+([a-zA-Z_][a-zA-Z_0-9]*)\s+([a-zA-Z_][a-zA-Z_0-9]*)\s*")
-
-def get_mangle_parameters(args, type_substititions: dict[str, Any], directives: dict[str, list[str]], elements: dict[str, list[str]]) -> dict[str, str]:
+def parse_mangles(args, types, source):
 
     mangles = {}
+    erase_regions = []
 
-    if 'MANGLE' not in directives or 'MANGLE' not in elements:
-        return mangles
+    def parser(m: re.Match[str]):
+        if m.group(1) != 'MANGLE':
+            return
 
-    for pattern, element in zip(directives['MANGLE'], elements['MANGLE']):
+        pattern = json.loads(m.group(2))
+        define = DEFINE.search(source, m.end())
+        if define is None:
+            print(f"MANGLE directive should annotate a #define at {lineo(m, source)}", file=stderr)
+            return
 
-        match = MANGLE.match(element)
-        if match is None:
-            print(f"Invalid template directive MANGLE={pattern}: {element}")
-            continue
+        erase_regions.append((m.start(), define.end()))
 
-        target = match.group(1)
-        base = match.group(2)
-        output = subst_env(pattern, {
-            "1": base,
-            **type_substititions,
+        subst_target = define.group(1)
+        subst_base = define.group(2)
+
+        subst_value = expand_env(pattern, {
+            "1": subst_base,
+            "<": subst_target,
+            **types,
         })
 
-        mangles[target] = output
+        mangles[subst_target] = subst_value
 
-    return mangles
-
-def map_type_parameters(args, types: dict[int, Any]) -> dict[str, Any]:
-    results = {}
-    for index, value in types.items():
-        if index > len(args.types):
-            print(f"Missing positional type parameter {index}, defaulting to int")
-            results[value] = "int"
-            continue
-        results[value] = args.types[index - 1]
-    return results
+    read_directives(source, parser)
+    return erase_all(source, erase_regions), mangles
 
 WHITESPACE = re.compile(r"\s+", flags=re.MULTILINE)
-CSTRING = re.compile(r'"(?:[^"]|\")*"')
-CCHAR = re.compile(r"'(?:[^']|\')*'")
+CSTRING = re.compile(r'"(?:[^"]|\\")*"')
+CCHAR = re.compile(r"'(?:[^']|\\')*'")
 CNAME = re.compile(r"[a-zA-Z_][a-zA-Z_0-9]*")
 CPREPROCESSOR = re.compile(r"\s*#.*$", flags=re.MULTILINE)
-CCOMMENT = re.compile(r"//.*$")
+CCOMMENT = re.compile(r"//.*$", flags=re.MULTILINE)
 CKEYWORD = re.compile(r"(?:alignas|alignof|auto|bool|break|case|char|const|constexpr|continue|default|do|double|else|enum|extern|false|float|for|goto|if|inline|int|long|nullptr|register|restrict|return|short|signed|sizeof|static|static_assert|struct|switch|thread_local|true|typedef|typeof|typeof_unqual|union|unsigned|void|volatile|while|_Alignas|_Alignof|_Atomic|_BitInt|_Bool|_Complex|_Decimal128|_Decimal32|_Decimal64|_Generic|_Imaginary|_Noreturn|_Static_assert|_Thread_local)")
 CBLOCKC = re.compile(r"/\*.*\*/", flags=re.MULTILINE)
 COTHER = re.compile(r"[^\sa-zA-Z_]+")
@@ -123,82 +189,49 @@ def token(source: str) -> tuple[str, str, str]:
     for kind, pattern in PATTERNS:
         m = re.match(pattern, source)
         if m is not None:
+            print(f"Token {kind=} {m.group(0)}")
             return kind, m.group(0), source[m.end():]
 
     return "NONE", "", source
 
-def substitute(source: str, type_substitutions: dict[str, Any], mangles: dict[str, str]) -> str:
+def substitute(source: str, mangles: dict[str, Any]) -> str:
     result = ""
-    next = source
+
     while True:
-        kind, tok, next = token(next)
+        kind, tok, next = token(source)
+        source = next
 
-        if tok in type_substitutions:
-            if type_substitutions[tok] is str:
-                result += type_substitutions[tok]
-            else:
-                result += type_substitutions[tok]["typename"]
-            continue
+        if tok == "":
+            result += next
+            break
 
-        if tok in mangles:
+        if kind == "CN" and tok in mangles:
             result += mangles[tok]
             continue
 
         result += tok
 
-        if next == "" or tok == "":
-            break
-
     return result
 
-def remove_directive(match: re.Match[str], source: str) -> str:
-    return source[:match.start()] + source[match.end():]
+def str_types(types: dict[str, Any]) -> dict[str, str]:
+    results = {}
 
-ATTRIBUTABLE = re.compile(r"(^\s*typedef\s+.+\s+[a-zA-Z_][a-zA-Z0-9_]*;$)|(^\s*#\s*define\s*[a-zA-Z_][a-zA-Z_0-9]+\s+.*$)", flags=re.MULTILINE)
+    for key, val in types.items():
+        if val is str:
+            results[key] = val
+        else:
+            results[key] = val["typename"]
 
-def process(args, input: str) -> str:
+    return results
 
-    directives = {}
-    elements = {}
-
-    def processor(directive: str, arguments: Any, match: re.Match[str], source: str) -> str:
-
-        mstr = match.group(0).strip()
-
-        if directive not in directives:
-            directives[directive] = []
-
-        directives[directive].append(arguments)
-
-        if directive in ATTRIBUTES:
-
-            attributed = ATTRIBUTABLE.search(source[match.end():])
-
-            if attributed is None:
-                print(f"Ignoring directive: {directive} {arguments}")
-                return remove_directive(match, source)
-
-            if directive not in elements:
-                elements[directive] = []
-
-            astr = attributed.group(0).strip()
-
-            elements[directive].append(astr)
-
-            if directive in HIDDEN:
-                return source[:match.start()] + source[match.end() + attributed.end():]
-
-        return remove_directive(match, source)
-
-    source = process_directives(input, processor)
-
-    type_params = get_type_parameters(args, directives, elements)
-
-    type_substititions = map_type_parameters(args, type_params)
-
-    mangles = get_mangle_parameters(args, type_substititions, directives, elements)
-
-    return substitute(source, type_substititions, mangles)
+def process(args):
+    with open(args.file) as f:
+        src = f.read()
+    data, types = parse_types(args, src)
+    data, mangles = parse_mangles(args, types, data)
+    data = substitute(data, str_types(types))
+    data = substitute(data, mangles)
+    print(data)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -252,9 +285,7 @@ def main():
                 args.types.append(t)
 
     try:
-        with open(args.file) as f:
-            data = f.read()
-        print(process(args, data))
+        process(args)
     except KeyboardInterrupt:
         exit(1)
 if __name__ == "__main__":
